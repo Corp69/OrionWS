4a703b33570133cdd3db6efa88de161f2de9f13c
Corpsey69 <ELIZANDRO@GMAIL.COM>
2024-10-30 21:33:31 -0600

    removemos las validaciones pipe

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,16 +6,6 @@ import { AuthModule } from './auth/auth.module';
   imports: [
       ConfigModule.forRoot(),
 
-      // TypeOrmModule.forRoot({
-      //   type: 'postgres',
-      //   host: process.env.DB_HOST,
-      //   port: +process.env.DB_PORT,
-      //   database: process.env.DB_NAME,
-      //   username: process.env.DB_USERNAME,
-      //   password: process.env.DB_PASSWORD,      
-      //   autoLoadEntities: true,
-      //   synchronize: true,
-      // }),
 
       AuthModule,
   ],
