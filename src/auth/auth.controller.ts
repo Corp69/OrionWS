d44bf0e0abfb4b085f9cb52fd1267dbc3e57c674
Corpsey69 <ELIZANDRO@GMAIL.COM>
2024-11-24 23:05:07 -0600

    Validando el JWT

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,16 @@
-import { 
-          Controller, 
-          Get, 
-          Post,
-          Body, 
-          Patch, 
-          Param, 
-          Delete 
-       }from '@nestjs/common';
+import { Controller, Get, Post, Body, UseGuards, Req, Headers, SetMetadata } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { IncomingHttpHeaders } from 'http';
+
 import { AuthService } from './auth.service';
-import { 
-        CreateUserDto,
-        LoginUserDto 
-       } from './dto';
+import { RawHeaders, GetUser, Auth } from './decorators';
+import { RoleProtected } from './decorators/role-protected.decorator';
+
+import { CreateUserDto, LoginUserDto } from './dto';
+import { Users } from './entities/users.entity';
+import { UserRoleGuard } from './guards/user-role.guard';
+import { ValidRoles } from './interfaces';
+
 
 
 
@@ -29,6 +28,57 @@ export class AuthController {
   loginUser(@Body() LoginUserDto: LoginUserDto) {
     return this.authService.login( LoginUserDto );
   }   
+
+
+  @Get('private')
+  @UseGuards( AuthGuard() )
+  testingPrivateRoute(
+            @Req() request: Express.Request,
+            @GetUser() user: Users,
+            @GetUser('email') userEmail: string,
+            
+            @RawHeaders() rawHeaders: string[],
+            @Headers() headers: IncomingHttpHeaders,
+  ) {
+    return {
+      ok: true,
+      message: 'Hola Mundo Private',
+      user,
+      userEmail,
+      rawHeaders,
+      headers
+    }
+  }
+
+
+    // @SetMetadata('roles', ['admin','super-user'])
+
+    @Get('private2')
+    @RoleProtected( ValidRoles.superUser, ValidRoles.admin )
+    @UseGuards( AuthGuard(), UserRoleGuard )
+    privateRoute2(
+      @GetUser() user: Users
+    ) {
+  
+      return {
+        ok: true,
+        user
+      }
+    }
+  
+  
+    @Get('private3')
+    @Auth( ValidRoles.admin )
+    privateRoute3(
+      @GetUser() user: Users
+    ) {
+  
+      return {
+        ok: true,
+        user
+      }
+    }
+
 
 
 }
