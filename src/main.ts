05d5d6c85f4310faeda9790aec6780f584f8cca2
Corpsey69 <ELIZANDRO@GMAIL.COM>
2024-12-06 18:31:51 -0600

    modificamos el cors

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   app.setGlobalPrefix('orionws');
 
       app.enableCors({
-        origin: ['http://localhost:4200', 'https://ec.arieserp.com.mx/'], // Dominios permitidos
+        origin: ['http://localhost:4200', 'https://ec.arieserp.com.mx'], // Dominios permitidos
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos HTTP permitidos
         credentials: true, // Permitir cookies u otras credenciales
         allowedHeaders: 'Content-Type, Authorization', // Encabezados permitidos
