3c7bf9da17533d9d608220090fed67e872d53b0b
AzureAD\ElizandroCarballoCas <elizandro.carballo@macropay.mx>
2025-01-15 18:25:52 -0600

    OrionWS - Scorpio XL - Modulos mapeados

---
+++ src/Scorpio/controlexportaciones/controlexportaciones.module.ts
@@ -0,0 +1,25 @@
+import { Module }                    from '@nestjs/common';
+import { ConfigModule }              from '@nestjs/config';
+import { TypeOrmModule }             from '@nestjs/typeorm';
+import { AuthModule }                from 'src/auth/auth.module';
+// Servicio Global
+import { DatabaseConnectionService } from '@shared/eccs/DatabaseConnectionService';
+
+@Module({
+    controllers: 
+    [ 
+
+    ],
+    providers:   
+    [ 
+      DatabaseConnectionService
+      
+    ],
+    imports: [
+        AuthModule,
+        ConfigModule,
+        TypeOrmModule.forFeature([])
+      ],
+    exports: [ TypeOrmModule ]
+})
+export class ControlexportacionesModule {}
