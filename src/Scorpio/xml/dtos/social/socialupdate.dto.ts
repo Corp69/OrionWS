5de8285e550555fa2572ad4cf010daced5e9bdb7
AzureAD\ElizandroCarballoCas <elizandro.carballo@macropay.mx>
2025-01-17 18:29:45 -0600

    OrionWS - Scorpio XL - XML - Razon social
    -- terminamos mapear los enpoints

--- src/Scorpio/xml/dtos/social/socialupdate.dto.ts
+++ src/Scorpio/xml/dtos/social/socialupdate.dto.ts
@@ -2,29 +2,45 @@ import { IsString, MaxLength, MinLength } from 'class-validator';
 export class SocialUpdateDto {
 
   @IsString() 
-  @MinLength(4)
-  @MaxLength(100)
+  @IsString({      message: 'userPade debe ser un String' })
+  @MinLength(3,  { message: 'userPade debe tener al menos 3 caracteres' })
+  @MaxLength(50, { message: 'userPade no debe exceder 50 caracteres' })
   readonly userPade:    string;
-  @IsString() 
-  @MinLength(4)
-  @MaxLength(100)
-  readonly passPade:    string;
-  @IsString() 
-  @MinLength(4)
-  @MaxLength(100)
-  readonly contrato:    string;
-  @IsString() 
-  @MinLength(4)
-  @MaxLength(100)
-  readonly rfc: string;
- 
- 
- 
- 
-  @IsString() 
-  @MinLength(4)
-  @MaxLength(100)
-  readonly razon_social: string;
-    
 
+  @IsString() 
+  @IsString({      message: 'passPade debe ser un String' })
+  @MinLength(3,  { message: 'passPade debe tener al menos 3 caracteres' })
+  @MaxLength(15, { message: 'passPade no debe exceder 15 caracteres' })
+  readonly passPade:    string;
+
+  @IsString() 
+  @IsString({      message: 'contrato debe ser un String' })
+  @MinLength(3,  { message: 'contrato debe tener al menos 3 caracteres' })
+  @MaxLength(30, { message: 'contrato no debe exceder 30 caracteres' })
+  readonly contrato:    string;
+
+  @IsString({      message: 'rfc debe ser un String' })
+  @MinLength(12, { message: 'rfc debe tener al menos 12 caracteres' })
+  @MaxLength(14, { message: 'rfc no debe exceder 14 caracteres' })
+  rfc: string;
+
+  @IsString({      message: 'password debe ser un String' })
+  @MinLength(12, { message: 'password debe tener al menos 12 caracteres' })
+  @MaxLength(50, { message: 'password no debe exceder 14 caracteres' })
+  password: string;
+  // Constructor para inicializar valores por defecto y objetos complejos
+  constructor(
+      userPade: string = "", 
+      passPade: string = "", 
+      contrato: string = "", 
+      rfc:      string = "",
+      password: string = ""
+    ) {
+    this.userPade = userPade;
+    this.passPade = passPade;
+    this.contrato = contrato;
+    this.rfc      = rfc;
+    this.password = password;
+  }
+  
 }
