095dea228421cc9422b6b10fb34721a8f66e49a7
AzureAD\ElizandroCarballoCas <elizandro.carballo@macropay.mx>
2025-01-20 17:43:27 -0600

    OrionWS - AriesERP - Modulo RH - Domicilios
    -- terminamos el endpoint de empleado domicilio

--- src/Aries/controlrh/controllers/empleadodomicilio/empleadodomicilio.controller.ts
+++ src/Aries/controlrh/controllers/empleadodomicilio/empleadodomicilio.controller.ts
@@ -1,4 +1,71 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Param, Post } from '@nestjs/common';
+import { ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Auth, GetUser } from 'src/auth/decorators';
+import { EmpleadoDomicilioService } from '../../services/empleadodomicilio/empleadodomicilio.service';
 
-@Controller('empleadodomicilio')
-export class EmpleadodomicilioController {}
+
+@ApiTags('OrionWS - AriesERP - Modulo RH.')
+@Controller('arieserp/empleadodomicilio')
+@Auth()
+export class EmpleadodomicilioController {
+
+    constructor(private readonly Service:  EmpleadoDomicilioService) {}
+
+    @Post('obtener/:id')
+    @ApiParam({
+          name: 'id',
+          description: 'Filtro: ID AriesERP - Modulo RH. Empleado Domicilios - estatus 1 =  Activos ',
+          required: true,
+          type: Number, // Especificamos que el tipo es un número
+    })
+    @ApiOperation({ summary: 'AriesERP - Modulo RH - Empleado domicilio - Obtener.' })
+    @ApiResponse({
+    status: 200,
+    description: 'AriesERP - Modulo RH - Empleado domicilio - Obtener.',
+    content: {
+        'application/json': {
+        example: {
+            Success: true,
+            Titulo:  'AriesERP - Modulo RH - Empleado domicilio - Obtener.',
+            Mensaje: 'Operacion Realizada con exito.',
+            Response: {
+                "empleadosdomicilio": [
+                    {
+                        "id": 1,
+                        "nombre": "ELIZANDRO",
+                        "calle": "Tuitlan",
+                        "num_ext": "138",
+                        "num_int": "",
+                        "cp": "22040",
+                        "pais": "MEXICO",
+                        "estatus": "Activo"
+                    }
+                ]
+            },
+        },
+        },
+    },
+    })
+    @ApiResponse({
+    status: 401,
+    description: 'AriesERP - Modulo RH - Empleado domicilio - Obtener.',
+    content: {
+        'application/json': {
+        example: {
+            message: 'No tienes Autorizacion.',
+            statusCode: 401,
+        },
+        },
+    },
+    })
+    @ApiResponse({ status: 404, description: 'Ruta no encontrada' })
+    @ApiResponse({ status: 401, description: 'Token Invalido' })
+    @ApiResponse({ status: 500, description: 'Error interno del servidor' })
+    public getConf(
+        @GetUser('id') idUser: number,
+        @Param('id')   id:     number
+    ) {
+    return this.Service.getEmpresa(idUser, id );
+    }
+
+}
--- src/Aries/controlrh/services/empleadodomicilio/empleadodomicilio.service.ts
+++ src/Aries/controlrh/services/empleadodomicilio/empleadodomicilio.service.ts
@@ -5,10 +5,34 @@ import { DatabaseConnectionService } from '@shared/eccs/DatabaseConnectionServic
 @Injectable()
 export class EmpleadoDomicilioService {
   
-  constructor( 
-     private readonly dbConnectionService: DatabaseConnectionService
-  ) {}
+  constructor(  private readonly dbConnectionService: DatabaseConnectionService ) {}
 
+  public async getEmpresa( clientId: number, _id_estatus: number ): Promise<ResponseDto<any>> {
+    try {
+      // Obtener la conexión adecuada según el cliente.
+      const connection = await this.dbConnectionService.getConnection(clientId);
+      //FUNCION
+      const data = await connection.query(
+        `select "arieserp_rh".fn_empleados_domicilio(${ _id_estatus })`,
+      );
+      return {
+        Success:  true,
+        Titulo:   'AriesERP - Modulo RH - Empleados - Domicilios.',
+        Mensaje:  'Operacion Realizada con exito.',
+        Response: data[0].fn_empleados_domicilio,
+      };
+    } catch (error) {
+      throw new HttpException(
+        {
+          Success:  false,
+          Titulo:   'AriesERP - Modulo RH - Empleados - Domicilios.',
+          Mensaje:  'Operación no se realizó',
+          Response: error.message || error,
+        },
+        HttpStatus.OK,
+      );
+    }
+  }
 
 
 }
