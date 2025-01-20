9cfad7e65402e8d956ec8c22ef12d1272b293120
AzureAD\ElizandroCarballoCas <elizandro.carballo@macropay.mx>
2025-01-20 17:16:29 -0600

    OrionWS - Scorpio XL - Configuraciones - obtener -- terminamos el endpoint de configuraciones

--- src/Scorpio/controlapp/controllers/configuraciones/configuraciones.controller.ts
+++ src/Scorpio/controlapp/controllers/configuraciones/configuraciones.controller.ts
@@ -1,4 +1,74 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
-@Controller('configuraciones')
-export class ConfiguracionesController {}
+//modulo auth
+import { Auth, GetUser } from 'src/auth/decorators';
+//Servicios
+import { ConfiguracionesService } from '../../services/configuraciones/configuraciones.service';
+
+
+@ApiTags('OrionWS - Scorpio XL - Modulo App - Configuraciones')
+@Controller('scorpio/configuraciones')
+@Auth()
+export class ConfiguracionesController {
+
+    constructor(private readonly Service: ConfiguracionesService) {}
+
+    @Post('obtener')
+    @ApiOperation({ summary: 'Scorpio XL - Modulo App - Configuraciones - Obtener.' })
+    @ApiResponse({
+    status: 200,
+    description: 'Scorpio XL - Modulo App - Configuraciones - Obtener.',
+    content: {
+        'application/json': {
+        example: {
+            Success: true,
+            Titulo:  'Scorpio XL - Modulo App - Configuraciones - Obtener.',
+            Mensaje: 'Operacion Realizada con exito.',
+            Response: {
+            "conf": [
+                {
+                    "id": 13,
+                    "decripcion": "Scorpio- Principal",
+                    "config": "scorpio - Tipo de cambio",
+                    "valor2": "https://openexchangerates.org/",
+                    "observaciones": "tipo de cambio por MXN",
+                    "activo": true,
+                    "icon": null
+                },
+                {
+                    "id": 12,
+                    "decripcion": "Scorpio- Principal",
+                    "config": "scorpio - Catalogo de bancos",
+                    "valor2": "https://www.banxico.org.mx/",
+                    "observaciones": "Esta configuracion trae el tipo de cambio de banxico",
+                    "activo": true,
+                    "icon": null
+                }
+            ]
+            },
+        },
+        },
+    },
+    })
+    @ApiResponse({
+    status: 401,
+    description: 'Scorpio XL - Modulo App - Empresas - Obtener.',
+    content: {
+        'application/json': {
+        example: {
+            message: 'No tienes Autorizacion.',
+            statusCode: 401,
+        },
+        },
+    },
+    })
+    @ApiResponse({ status: 404, description: 'Ruta no encontrada' })
+    @ApiResponse({ status: 401, description: 'Token Invalido' })
+    @ApiResponse({ status: 500, description: 'Error interno del servidor' })
+    public getConf(@GetUser('id') idUser: number) {
+        return this.Service.getConfiguracion(idUser);
+    }
+
+
+}
--- src/Scorpio/controlapp/services/configuraciones/configuraciones.service.ts
+++ src/Scorpio/controlapp/services/configuraciones/configuraciones.service.ts
@@ -1,28 +1,22 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { ResponseDto } from 'src/shared/dtos/Response.dto';
-
 // datasource
 import { DatabaseConnectionService } from 'src/shared/eccs/DatabaseConnectionService';
-// Dtos
-import { EmpresasDTO } from '../../dtos/empresa/empresas.dto';
-// entidad
-import { scorpio_empresa } from '../../entities/empresa/scorpio_empresa.entity';
 
 @Injectable()
 export class ConfiguracionesService {
   
-  constructor(
-    private readonly dbConnectionService: DatabaseConnectionService
-  ) {}
+  constructor( private readonly dbConnectionService: DatabaseConnectionService) {}
+
   public async getConfiguracion( clientId: number ): Promise<ResponseDto<any>> {
     try {
       // Obtener la conexión adecuada según el cliente.
       const connection = await this.dbConnectionService.getConnection(clientId);
       //FUNCION
-      const data = await connection.query(`SELECT "scorpio".app_empresas()`);
+      const data = await connection.query(`SELECT "scorpio".scorpio_configuraciones()`);
       return {
         Success:  true,
-        Titulo:   'Scorpio XL - Modulo App - Configuraciones.',
+        Titulo:   'Scorpio XL - Modulo App - Configuraciones - Obtener',
         Mensaje:  'Operacion Realizada con exito.',
         Response: data[0].scorpio_configuraciones,
       };
@@ -30,7 +24,7 @@ export class ConfiguracionesService {
       throw new HttpException(
         {
           Success:  false,
-          Titulo:   'Scorpio XL- Modulo App - Configuraciones.',
+          Titulo:   'Scorpio XL- Modulo App - Configuraciones - Obtener.',
           Mensaje:  'Operación no se realizó',
           Response: error.message || error,
         },
