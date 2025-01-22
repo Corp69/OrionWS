c541fcecd9338d284a88cfd612534a213cf738bd
Ricardo <pizanadesantiagoricardo@gmail.com>
2025-01-22 11:13:12 -0600

    OrionWS - ARIESERP - ControlRH -- empleado Domicilio: -- Se creo controlador y servicio de empleados domicilio

--- src/Aries/controlrh/controllers/empleadodomicilio/empleadodomicilio.controller.ts
+++ src/Aries/controlrh/controllers/empleadodomicilio/empleadodomicilio.controller.ts
@@ -1,8 +1,53 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Auth, GetUser } from 'src/auth/decorators';
+import { EmpleadoDomicilioService } from '../../services/empleadodomicilio/empleadodomicilio.service';
 
+
+
+@ApiTags('OrionWS - AriesERP - Modulo RH - Obtener Empleado Domicilio.')
 @Controller('empleadodomicilio')
+@Auth()
 export class EmpleadodomicilioController {
 
+    constructor(private readonly Service: EmpleadoDomicilioService) {}
+
+        @Get('obtener')
+              @ApiOperation({ summary: 'AriesERP - Modulo RH - Obtener Empleado Domicilio.' })
+              @ApiResponse({
+                status: 200,
+                description: 'AriesERP - Modulo RH - Obtener Empleado Domicilio.',
+                content: {
+                  'application/json': {
+                    example: {
+                      Success: true,
+                      Titulo:  'AriesERP - Modulo RH - Obtener Empleado Domicilio.',
+                      Mensaje: 'Operacion Realizada con exito.',
+                      Response: {
+                        
+                      },
+                    },
+                  },
+                },
+              })
+              @ApiResponse({
+                status: 401,
+                description: 'AriesERP - Modulo RH - Obtener Empleado Domicilio.',
+                content: {
+                  'application/json': {
+                    example: {
+                      message: 'No tienes Autorizacion.',
+                      statusCode: 401,
+                    },
+                  },
+                },
+              })
+              @ApiResponse({ status: 404, description: 'Ruta no encontrada' })
+              @ApiResponse({ status: 401, description: 'Token Invalido' })
+              @ApiResponse({ status: 500, description: 'Error interno del servidor' })
+              public getConf(@GetUser('id') idUser: number) {
+                return this.Service.getEmpleadoDomicilio(idUser);
+              }
     
 
 }
--- src/Aries/controlrh/services/empleadodomicilio/empleadodomicilio.service.ts
+++ src/Aries/controlrh/services/empleadodomicilio/empleadodomicilio.service.ts
@@ -9,6 +9,31 @@ export class EmpleadoDomicilioService {
      private readonly dbConnectionService: DatabaseConnectionService
   ) {}
 
-
+  public async getEmpleadoDomicilio( EmpleadoDomicilioID: number ): Promise<ResponseDto<any>> {
+    try {
+      // Obtener la conexión adecuada según el cliente.
+      const connection = await this.dbConnectionService.getConnection(EmpleadoDomicilioID);
+      //FUNCION
+      const data = await connection.query(
+        `select "arieserp_rh".fn_empleados_domicilio()`,
+      );
+      return {
+        Success:  true,
+        Titulo:   'AriesERP - Modulo RH - Obtener Empleado Domicilio.',
+        Mensaje:  'Operacion Realizada con exito.',
+        Response: data[0].arieserp_rh,
+      };
+    } catch (error) {
+      throw new HttpException(
+        {
+          Success:  false,
+          Titulo:   'AriesERP - Modulo RH - Obtener Empleado Domicilio.',
+          Mensaje:  'Operación no se realizó',
+          Response: error.message || error,
+        },
+        HttpStatus.OK,
+      );
+    }
+  }
 
 }
