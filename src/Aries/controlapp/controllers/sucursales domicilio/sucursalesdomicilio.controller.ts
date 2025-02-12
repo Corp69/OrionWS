8d69231ced96484463079a13c69ac79242a3ddfc
Corpsey69 <elizandro.carballo.casillas@eccs.com.mx>
2025-02-12 07:28:38 -0600

    OrionWS - Control App  - Sucursal --- terminamos funcionalidad de sucursal y agregamos ID en agregar y actualizar en response

--- src/Aries/controlapp/controllers/sucursales domicilio/sucursalesdomicilio.controller.ts
+++ src/Aries/controlapp/controllers/sucursales domicilio/sucursalesdomicilio.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post} from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { Auth, GetUser } from 'src/auth/decorators';
 
@@ -7,137 +7,138 @@ import { SucursalesDomiciliosService } from '../../services/sucursalesdomicilio/
 //dtos
 import { DomicilioSucursalDTO } from '../../dtos/arieserpsucursaldomicilio.dto';
 
-
-
 @ApiTags('OrionWS - AriesERP - Modulo App - Sucursales Domicilio.')
 @Controller('arieserp/sucursalesdomicilio')
 @Auth()
 export class SucursalesDomicilioController {
+  constructor(private readonly Service: SucursalesDomiciliosService) {}
 
-    constructor(private readonly Service: SucursalesDomiciliosService) {}
-
-
-
-    
-    @Get('obtener/:id')
-    @ApiOperation({ summary: 'OrionWS - AriesERP - Modulo App - Sucursales Domiclio - Obtener.' })
-    @ApiResponse({
-      status: 200,
-      description: 'OrionWS - AriesERP - Modulo App - Sucursales Domiclio - Obtener.',
-      content: {
-        'application/json': {
-          example: {
-            Success: true,
-            Titulo:  'OrionWS - AriesERP - Modulo App - Sucursales Domiclio - Obtener.',
-            Mensaje: 'Operacion Realizada con exito.',
-            Response: {
-              
-            },
-          },
+  @Get('obtener/:id')
+  @ApiOperation({
+    summary: 'OrionWS - AriesERP - Modulo App - Sucursales Domiclio - Obtener.',
+  })
+  @ApiResponse({
+    status: 200,
+    description:
+      'OrionWS - AriesERP - Modulo App - Sucursales Domiclio - Obtener.',
+    content: {
+      'application/json': {
+        example: {
+          Success: true,
+          Titulo:
+            'OrionWS - AriesERP - Modulo App - Sucursales Domiclio - Obtener.',
+          Mensaje: 'Operacion Realizada con exito.',
+          Response: {},
         },
       },
-    })
-    @ApiResponse({
-      status: 401,
-      description: 'OrionWS - AriesERP - Modulo App - Sucursales Domiclio - Obtener.',
-      content: {
-        'application/json': {
-          example: {
-            message: 'No tienes Autorizacion.',
-            statusCode: 401,
-          },
+    },
+  })
+  @ApiResponse({
+    status: 401,
+    description:
+      'OrionWS - AriesERP - Modulo App - Sucursales Domiclio - Obtener.',
+    content: {
+      'application/json': {
+        example: {
+          message: 'No tienes Autorizacion.',
+          statusCode: 401,
         },
       },
-    })
-    @ApiResponse({ status: 404, description: 'Ruta no encontrada' })
-    @ApiResponse({ status: 401, description: 'Token Invalido' })
-    @ApiResponse({ status: 500, description: 'Error interno del servidor' })
-    public getConf(@GetUser('id') idUser: number, @Param('id') id: number) {
-      return this.Service.obtenerDomicilioSucursal(idUser, id);
-    }
-
+    },
+  })
+  @ApiResponse({ status: 404, description: 'Ruta no encontrada' })
+  @ApiResponse({ status: 401, description: 'Token Invalido' })
+  @ApiResponse({ status: 500, description: 'Error interno del servidor' })
+  public getConf(@GetUser('id') idUser: number, @Param('id') id: number) {
+    return this.Service.obtenerDomicilioSucursal(idUser, id);
+  }
 
   @Post('agregar')
-    @ApiOperation({
-      summary: 'OrionWS - AriesERP - Modulo App - Sucursales Domiclio - Agregar',
-    })
-    @ApiResponse({
-      status: 200,
-      description: 'OrionWS - AriesERP - Modulo App - Sucursales Domiclio - Agregar',
-      content: {
-        'application/json': {
-          example: {
-            Success:  true,
-            Titulo:   "OrionWS - AriesERP - Modulo App - Sucursales Domicilio - Agregar",
-            Mensaje:  "Operación Realizada con exito.",
-            Response: "Se agrego correctamente!!"
-          },
+  @ApiOperation({
+    summary: 'OrionWS - AriesERP - Modulo App - Sucursales Domiclio - Agregar',
+  })
+  @ApiResponse({
+    status: 200,
+    description:
+      'OrionWS - AriesERP - Modulo App - Sucursales Domiclio - Agregar',
+    content: {
+      'application/json': {
+        example: {
+          Success: true,
+          Titulo:
+            'OrionWS - AriesERP - Modulo App - Sucursales Domicilio - Agregar',
+          Mensaje: 'Operación Realizada con exito.',
+          Response: 'Se agrego correctamente!!',
         },
       },
-    })
-    @ApiResponse({
-      status: 401,
-      description: 'OrionWS - AriesERP - Modulo App - Sucursales Domiclio - Agregar',
-      content: {
-        'application/json': {
-          example: {
-            message: 'No tienes Autorizacion.',
-            statusCode: 401,
-          },
+    },
+  })
+  @ApiResponse({
+    status: 401,
+    description:
+      'OrionWS - AriesERP - Modulo App - Sucursales Domiclio - Agregar',
+    content: {
+      'application/json': {
+        example: {
+          message: 'No tienes Autorizacion.',
+          statusCode: 401,
         },
       },
-    })
-    @ApiResponse({ status: 404, description: 'Ruta no encontrada' })
-    @ApiResponse({ status: 401, description: 'Token Invalido' })
-    @ApiResponse({ status: 500, description: 'Error interno del servidor' })
-    public Crear(
-      @Body() DomicilioSucursalDTO: DomicilioSucursalDTO,
-      @GetUser('id') idUser: number,
-    ) {
-      return this.Service.Agregar(idUser, DomicilioSucursalDTO);
-    }
+    },
+  })
+  @ApiResponse({ status: 404, description: 'Ruta no encontrada' })
+  @ApiResponse({ status: 401, description: 'Token Invalido' })
+  @ApiResponse({ status: 500, description: 'Error interno del servidor' })
+  public Crear(
+    @Body() DomicilioSucursalDTO: DomicilioSucursalDTO,
+    @GetUser('id') idUser: number,
+  ) {
+    return this.Service.Agregar(idUser, DomicilioSucursalDTO);
+  }
 
   @Post('actualizar')
-    @ApiOperation({
-      summary: "AriesERP - Modulo Compras - Sucursales Domicilio - Actualizar",
-    })
-    @ApiResponse({
-      status: 200,
-      description: "AriesERP - Modulo Compras - Sucursales Domicilio - Actualizar.",
-      content: {
-        'application/json': {
-          example: {
-            Success:  true,
-            Titulo:   "AriesERP - Modulo Compras - Sucursales Domicilio - Actualizar",
-            Mensaje:  "Operacion Realizada con exito.",
-            Response: "Se actualizo correctamente!!"
-          },
+  @ApiOperation({
+    summary: 'AriesERP - Modulo Compras - Sucursales Domicilio - Actualizar',
+  })
+  @ApiResponse({
+    status: 200,
+    description:
+      'AriesERP - Modulo Compras - Sucursales Domicilio - Actualizar.',
+    content: {
+      'application/json': {
+        example: {
+          Success: true,
+          Titulo:
+            'AriesERP - Modulo Compras - Sucursales Domicilio - Actualizar',
+          Mensaje: 'Operacion Realizada con exito.',
+          Response: 'Se actualizo correctamente!!',
         },
       },
-    })
-    @ApiResponse({
-      status: 401,
-      description: 'AriesERP - Modulo Compras - Sucursales Domicilio - Actualizar.',
-      content: {
-        'application/json': {
-          example: {
-            message: 'No tienes Autorizacion.',
-            statusCode: 401,
-          },
+    },
+  })
+  @ApiResponse({
+    status: 401,
+    description:
+      'AriesERP - Modulo Compras - Sucursales Domicilio - Actualizar.',
+    content: {
+      'application/json': {
+        example: {
+          message: 'No tienes Autorizacion.',
+          statusCode: 401,
         },
       },
-    })
-    @ApiResponse({ status: 404, description: 'Ruta no encontrada' })
-    @ApiResponse({ status: 401, description: 'Token Invalido' })
-    @ApiResponse({ status: 500, description: 'Error interno del servidor' })
-    public Actualizar(
-      @Body() DomicilioSucursalDTO: DomicilioSucursalDTO,
-      @GetUser('id') idUser: number,
-    ) {
-      return this.Service.Actualizar(idUser, DomicilioSucursalDTO);
-    }
-          
-            
+    },
+  })
+  @ApiResponse({ status: 404, description: 'Ruta no encontrada' })
+  @ApiResponse({ status: 401, description: 'Token Invalido' })
+  @ApiResponse({ status: 500, description: 'Error interno del servidor' })
+  public Actualizar(
+    @Body() DomicilioSucursalDTO: DomicilioSucursalDTO,
+    @GetUser('id') idUser: number,
+  ) {
+    return this.Service.Actualizar(idUser, DomicilioSucursalDTO);
+  }
+
   @Post('eliminar/:id')
   @ApiOperation({
     summary: 'AriesERP - Modulo Compras - Sucursales Domicilio - Eliminar',
@@ -149,9 +150,9 @@ export class SucursalesDomicilioController {
       'application/json': {
         example: {
           Success: true,
-          Titulo:  'AriesERP - Modulo Compras - Sucursales Domicilio - Eliminar',
+          Titulo: 'AriesERP - Modulo Compras - Sucursales Domicilio - Eliminar',
           Mensaje: 'Operacion Realizada con exito.',
-          Response: "Registro eliminado."
+          Response: 'Registro eliminado.',
         },
       },
     },
@@ -171,11 +172,7 @@ export class SucursalesDomicilioController {
   @ApiResponse({ status: 404, description: 'Ruta no encontrada' })
   @ApiResponse({ status: 401, description: 'Token Invalido' })
   @ApiResponse({ status: 500, description: 'Error interno del servidor' })
-  public eliminar(
-    @GetUser('id') idUser: number,
-    @Param('id')   id:     number
-  ) {
+  public eliminar(@GetUser('id') idUser: number, @Param('id') id: number) {
     return this.Service.Eliminar(idUser, id);
   }
-
 }
