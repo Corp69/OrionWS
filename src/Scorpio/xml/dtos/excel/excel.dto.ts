a236be0aa0f9c9528ce0597dce2193fa90a11df5
AzureAD\ElizandroCarballoCas <elizandro.carballo@macropay.mx>
2025-04-15 12:07:37 -0600

    OrionWS - Scorpio xl - xml - excel
    -- comenzamos a realizar la consulta de excel listo para pruebas

--- src/Scorpio/xml/dtos/excel/excel.dto.ts
+++ src/Scorpio/xml/dtos/excel/excel.dto.ts
@@ -4,26 +4,26 @@ import { IsDate, IsNumber, IsString, Matches, Max, MAX, MaxLength, Min, MinLengt
 export class excelDto {
 
   @ApiProperty({
-    example: '01-01-2025',
+    example: '2025-01-01',
     description: '_fechainicio: para realizar el inicio del rango a consultar',
   })
   @IsString({ message: '_fechainicio debe ser un string' })
   @MinLength(10, { message: '_fechainicio debe tener exactamente 10 caracteres' })
   @MaxLength(10, { message: '_fechainicio debe tener exactamente 10 caracteres' })
-  @Matches(/^\d{2}-\d{2}-\d{4}$/, {
-    message: '_fechainicio debe tener el formato DD-MM-YYYY',
+  @Matches(/^\d{4}-\d{2}-\d{2}$/, {
+    message: '_fechainicio debe tener el formato YYYY-MM-DD',
   })
   readonly _fechainicio: string;
 
   @ApiProperty({
-    example: '01-01-2025',
+    example: '2025-01-01',
     description: '_fechatermina: para realizar el inicio del rango a consultar',
   })
   @IsString({ message: '_fechatermina debe ser un string' })
   @MinLength(10, { message: '_fechatermina debe tener exactamente 10 caracteres' })
   @MaxLength(10, { message: '_fechatermina debe tener exactamente 10 caracteres' })
-  @Matches(/^\d{2}-\d{2}-\d{4}$/, {
-    message: '_fechatermina debe tener el formato DD-MM-YYYY',
+  @Matches(/^\d{4}-\d{2}-\d{2}$/, {
+    message: '_fechatermina debe tener el formato YYYY-MM-DD',
   })
   readonly _fechatermina: string;
 
@@ -35,5 +35,14 @@ export class excelDto {
   @Min(1, { message: 'id_tipo debe ser mayor o igual a 1' })
   @Max(5, { message: 'id_tipo no debe ser mayor a 5' })
   readonly _id_tipo: number;
+
+  @ApiProperty({
+    example: 41,
+    description: '_id_empresa: debe ser un número.',
+  })
+  @IsNumber({}, { message: 'id_tipo debe ser un número' })
+  @Min(1, { message: '_id_empresa debe ser mayor a 1 este debe estar registrado en BD' })
+  @Max(999, { message: '_id_empresa no debe excer a 1000' })
+  readonly _id_empresa: number;
 
 }
